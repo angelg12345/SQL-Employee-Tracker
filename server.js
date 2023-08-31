d31ea341a11c7c2c977e9431120aa7e78f835389
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-08-31 15:16:45 -0700

    started with schema sql. the tables i needed with department role and employee.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const mysql = require('mysql2')
-const inquirer = require('inquirer')
-const express = require('express')
-
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const express = require('express');
+const cTable = require('console.table');
+require('dotenv')
